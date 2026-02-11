0e64412b18c3332999a0ea05a45f16f11cf91382
Devoura Studio <linkedinmiran@gmail.com>
2026-02-11 15:35:06 +0330

    feat(views): add boilerplate layout template using ejs-mate

--- seeds/app.js
+++ seeds/app.js
@@ -8,22 +8,22 @@ mongoose.connect("mongodb://localhost:27017/Yelpcamp", {
 	useCreateIndex: true,
 	useUnifiedTopology: true
 })
-.then(() => {
-	console.log("Database is running!")
-})
-.catch((err) => {
-console.log("Database is not running!!", err)
-})
+	.then(() => {
+		console.log("Database is running!")
+	})
+	.catch((err) => {
+		console.log("Database is not running!!", err)
+	})
 const sample = array => array[Math.floor(Math.random() * array.length)]
 const seedDB = async () => {
 	await Campground.deleteMany({})
 	for (let i = 0; i < 50; i++) {
-	const random1000 = Math.floor(Math.random() * 1000)
-	const camp = new Campground({
-		location: `${cities[random1000].city}, ${cities[random1000].state}`,
-		title: `${sample(descriptors)} ${sample(places)}`
-	})
-	await camp.save()
+		const random1000 = Math.floor(Math.random() * 1000)
+		const camp = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${sample(descriptors)} ${sample(places)}`
+		})
+		await camp.save()
 	}
 }
 seedDB().then(() => mongoose.connection.close())
