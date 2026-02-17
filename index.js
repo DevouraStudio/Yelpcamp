0bfaa4d730ac9ed0694f23354bc292549ed1cdd9
Devoura Studio <linkedinmiran@gmail.com>
2026-02-17 16:15:06 +0330

    Validate form data on the server side using joi package.

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const catchAsync = require("./utilities/catchAsync")
 const ExpressError = require("./utilities/ExpressError")
+const { campgroundSchema } = require("./schemas.js")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.engine("ejs", ejsMate)
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
 mongoose.connect("mongodb://localhost:27017/Yelpcamp", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
@@ -23,6 +22,15 @@ mongoose.connect("mongodb://localhost:27017/Yelpcamp", {
 	.catch((err) => {
 		console.log(err, "Database is not running!!")
 	})
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+const validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body)
+	if (error) {
+		const msg = error.details.map(el => el.message).join(",")
+		throw new ExpressError(msg, 400)
+	}
+}
 app.get("/campgrounds", catchAsync(async (req, res) => {
 	const campgrounds = await Campground.find({})
 	res.render("campgrounds/index", { campgrounds })
@@ -34,8 +42,7 @@ app.get("/campgrounds/:id", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render("campgrounds/show", { campground })
 }))
-app.post("/campgrounds", catchAsync(async (req, res) => {
-	if(!req.body.campground) throw new ExpressError("Invalid campground data!", 400)
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res) => {
 	const campground = new Campground(req.body.campground)
 	await campground.save()
 	res.redirect(`/campgrounds/${campground._id}`)
@@ -44,7 +51,7 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render("campgrounds/edit", { campground })
 }))
-app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
 	const campground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, { useFindAndModify: false })
 	await campground.save()
 	res.redirect(`/campgrounds/${campground._id}`)
