67ed64718c247fda1da166fb290146f9240ecbc5
Devoura Studio <linkedinmiran@gmail.com>
2026-06-04 12:15:11 +0330

    refactor(controllers): extract route handlers into dedicated controller files

---
+++ controllers/campgrounds.js
@@ -0,0 +1,55 @@
+const Campground = require("../models/campground")
+
+module.exports.index = async (req, res) => {
+	const campgrounds = await Campground.find({}).sort({ _id: 1 })
+	res.render("campgrounds/index", { campgrounds })
+}
+
+module.exports.renderNewForm = (req, res) => {
+	res.render("campgrounds/new")
+}
+
+module.exports.showCampground = async (req, res) => {
+	const campground = await Campground.findById(req.params.id).populate({
+		path: "reviews",
+		populate: {
+			path: "author"
+		}
+	}).populate("author")
+	console.log(campground)
+	if (!campground) {
+		req.flash("error", "Cannot find that campground!")
+		return res.redirect("/campgrounds")
+	}
+	res.render("campgrounds/show", { campground })
+}
+
+module.exports.createCampground = async (req, res) => {
+	const campground = new Campground(req.body.campground)
+	campground.author = req.user._id
+	await campground.save()
+	req.flash("success", "Successfully made a new campground!")
+	res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.renderEditForm = async (req, res) => {
+	const campground = await Campground.findById(req.params.id)
+	if (!campground) {
+		req.flash("error", "Cannot find that campground!")
+		return res.redirect("/campgrounds")
+	}
+	res.render("campgrounds/edit", { campground })
+}
+
+module.exports.updateCampground = async (req, res) => {
+	const campground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, { useFindAndModify: false })
+	await campground.save()
+	req.flash("success", "Campground updated successfully!")
+	res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.deleteCampground = async (req, res) => {
+	await Campground.findByIdAndDelete(req.params.id, { useFindAndModify: false })
+	req.flash("success", "Campground deleted successfully!")
+	res.redirect("/campgrounds")
+}
