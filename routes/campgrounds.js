efb2c9a0a04055a966613ebfdb8e896d277cb5ae
Devoura Studio <linkedinmiran@gmail.com>
2026-06-15 17:00:23 +0330

    feat(ui): add image upload input to campground edit page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,13 +12,13 @@ const upload = require("../Arvancloud")
 
 router.route("/")
 	.get(catchAsync(campgrounds.index))
-	.post(isLoggedIn, upload.array("image", 15), validateCampground, catchAsync(campgrounds.createCampground))
+	.post(isLoggedIn, upload.array("image", 10), validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm)
 
 router.route("/:id")
 	.get(catchAsync(campgrounds.showCampground))
-	.put(validateCampground, isLoggedIn, isAuthor, catchAsync(campgrounds.updateCampground))
+	.put(isLoggedIn, isAuthor, upload.array("image", 10), validateCampground, catchAsync(campgrounds.updateCampground))
 	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
