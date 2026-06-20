c3e83e3ce6ae13ebfb342e335cc32af155735465
Devoura Studio <linkedinmiran@gmail.com>
2026-06-20 19:23:30 +0330

    fix(seeds): catch unhandled promise rejections in seedDB

--- seeds/app.js
+++ seeds/app.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
+
 const { places, descriptors } = require("./seedHelpers")
+
 const cities = require("./cities")
+
 const Campground = require("../models/campground")
+
 const axios = require("axios")
 
 mongoose.connect("mongodb://localhost:27017/Yelpcamp", {
@@ -15,14 +19,18 @@ mongoose.connect("mongodb://localhost:27017/Yelpcamp", {
 	.catch((err) => {
 		console.log("Database is not running!!", err)
 	})
+
 const sample = array => array[Math.floor(Math.random() * array.length)]
+
 const seedDB = async () => {
 	await Campground.deleteMany({})
 	for (let i = 0; i < 50; i++) {
 		const [res, res2] = await Promise.all([
-		axios.get("https://random.imagecdn.app/v1/image?width=500&height=500&category=nature&format=text"),
-		axios.get("https://random.imagecdn.app/v1/image?width=500&height=500&category=nature&format=text")
-		])
+			axios.get("https://random.imagecdn.app/v1/image?width=500&height=500&category=nature&format=text"),
+			axios.get("https://random.imagecdn.app/v1/image?width=500&height=500&category=nature&format=text")
+		]).catch((err) => {
+			console.log(err, "The image API is not working propely!")
+		})
 		const random1000 = Math.floor(Math.random() * 1000)
 		const randomPrice = Math.floor(Math.random() * 20) + 10
 		const camp = new Campground({
@@ -45,4 +53,5 @@ const seedDB = async () => {
 		await camp.save()
 	}
 }
+
 seedDB().then(() => mongoose.connection.close())
