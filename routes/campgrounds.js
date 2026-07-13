f82446da24c1a5fea99fc4bb41a25cb7eb213c10
Devoura Studio <linkedinmiran@gmail.com>
2026-07-13 21:59:50 +0330

    feat(campgrounds): add flash messages to campground and review routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,24 +21,35 @@ router.get("/new", (req, res) => {
 })
 router.get("/:id", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id).populate("reviews")
+	if (!campground) {
+		req.flash("error", "Cannot find that campground!")
+		return res.redirect("/campgrounds")
+	}
 	res.render("campgrounds/show", { campground })
 }))
 router.post("/", validateCampground, catchAsync(async (req, res) => {
 	const campground = new Campground(req.body.campground)
 	await campground.save()
+	req.flash("success", "Successfully made a new campground!")
 	res.redirect(`/campgrounds/${campground._id}`)
 }))
 router.get("/:id/edit", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
+	if (!campground) {
+		req.flash("error", "Cannot find that campground!")
+		return res.redirect("/campgrounds")
+	}
 	res.render("campgrounds/edit", { campground })
 }))
 router.put("/:id", validateCampground, catchAsync(async (req, res) => {
 	const campground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, { useFindAndModify: false })
 	await campground.save()
+	req.flash("success", "Campground updated successfully!")
 	res.redirect(`/campgrounds/${campground._id}`)
 }))
 router.delete("/:id", catchAsync(async (req, res) => {
 	await Campground.findByIdAndDelete(req.params.id, { useFindAndModify: false })
+	req.flash("success", "Campground deleted successfully!")
 	res.redirect("/campgrounds")
 }))
 module.exports = router
