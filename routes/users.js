abdb62e9ccd289f16f6ef2108f218a633e6ef59d
Devoura Studio <linkedinmiran@gmail.com>
2026-07-16 20:13:29 +0330

    feat(auth): implement login functionality routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 const catchAsync = require("../utilities/catchAsync")
+const passport = require("passport")
 router.get("/register", (req, res) => {
 	res.render("users/register")
 })
@@ -17,4 +18,11 @@ router.post("/register", catchAsync(async (req, res) => {
 	req.flash("success", "Welcome to Yelpcamp!")
 	res.redirect("/campgrounds")
 }))
+router.get("/login", (req, res) => {
+	res.render("users/login")
+})
+router.post("/login", passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}) ,(req, res) => {
+	req.flash("success", "Welcome back to Yelpcamp!")
+	res.redirect("/campgrounds")
+})
 module.exports = router
