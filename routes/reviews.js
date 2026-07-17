e931d8600695439383a48a2cb847e2103deb7d64
Devoura Studio <linkedinmiran@gmail.com>
2026-07-17 15:37:21 +0330

    feat(auth): import middlewares and protect review routes with validateReview and isReviewAuthor middlewares

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,27 +3,19 @@ const router = express.Router({ mergeParams: true })
 const catchAsync = require("../utilities/catchAsync")
 const Campground = require("../models/campground")
 const Review = require("../models/review")
-const { reviewSchema } = require("../schemas.js")
-const ExpressError = require("../utilities/ExpressError")
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body)
-	if (error) {
-		const msg = error.details.map(el => el.message).join(",")
-		throw new ExpressError(msg, 400)
-	}
-	next()
-}
-router.post("/", validateReview, catchAsync(async (req, res) => {
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middlewares")
+router.post("/", validateReview, isLoggedIn, catchAsync(async (req, res) => {
 	const { id } = req.params
 	const campground = await Campground.findById(id)
 	const review = await new Review(req.body.review)
+	review.author = req.user._id
 	campground.reviews.push(review)
 	await campground.save()
 	await review.save()
 	req.flash("success", "Successfully made a new review!")
 	res.redirect(`/campgrounds/${id}`)
 }))
-router.delete("/:reviewId", catchAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
 	const { id, reviewId } = req.params
 	await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }, { useFindAndModify: false })
 	await Review.findByIdAndDelete(reviewId)
