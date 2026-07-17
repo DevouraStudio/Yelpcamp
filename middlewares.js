10fe7c77528ff1c2cc1189bbde828f51672977b4
Devoura Studio <linkedinmiran@gmail.com>
2026-07-17 14:41:29 +0330

    feat(auth): add isAuthor middleware to authorize authors when hitting campground edit/delete routes

--- middlewares.js
+++ middlewares.js
@@ -17,3 +17,12 @@ module.exports.isLoggedIn = (req, res, next) => {
 	}
 	next()
 }
+module.exports.isAuthor = async(req, res, next) => {
+	const { id } = req.params
+	const campground = await Campground.findById(id)
+	if (!campground.author.equals(req.user._id)) {
+		req.flash("error", "You do not have any permission to do that!")
+		res.redirect(`/campgrounds/${id}`)
+	}
+	next()
+}
