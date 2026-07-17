3f134590d151cefa4e769c1a8ca7c53b183c3f8f
Devoura Studio <linkedinmiran@gmail.com>
2026-07-17 14:34:57 +0330

    refactor(middleware): move validateCampground middleware to middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,14 @@
+const Campground = require("./models/campground")
+const { campgroundSchema } = require("./schemas.js")
+const ExpressError = require("./utilities/ExpressError")
+module.exports.validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body)
+	if (error) {
+		const msg = error.details.map(el => el.message).join(",")
+		throw new ExpressError(msg, 400)
+	}
+	next()
+}
 module.exports.isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
 		req.session.returnTo = req.originalUrl
